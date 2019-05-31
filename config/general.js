21a70d07b49ee0d35d1fcaa436ff76bec25d5a59
User65536 <mengxiangji365@163.com>
2019-05-31 11:37:57 +0800

    配置项文件

---
+++ config/general.js
@@ -0,0 +1,4 @@
+module.exports = {
+  imgDir: '/Users/trage/desktop/project/joinor-微活动/back-end/public',
+  host: 'http://localhost:8000/'
+}
---
+++ config/mpcode.js
@@ -0,0 +1,4 @@
+module.exports = {
+  appID: 'wxab16bc3c3a604795',
+  appSecret: '00e8a84fa441559a920cc623f9eb82b3'
+}
